285595d4b7024aa34cdca98d1b5ebbf6253d4bd2
valentinedwv <david.valentine@gmail.com>
2015-02-09 10:32:39 -0800

    Add notes and a basic validation

--- src/python/czo_yaml/display_file.py
+++ src/python/czo_yaml/display_file.py
@@ -11,6 +11,7 @@ import genson
 import json
 from urllib2 import urlopen
 import json
+from jsonschema import validate, ValidationError
 
 logging.basicConfig(level=logging.DEBUG)
 console = logging.StreamHandler()
@@ -31,7 +32,27 @@ logger.addHandler(filelogger)
 def display_file():
     pass
 
-def df_validate(json_schemas,stream ):
+def df_validate(args ):
+
+    level = args.level
+
+    validation_log =["validation starting"]
+    stream = file(args.source)
+
+    ymal_data = yaml.load (stream)
+
+    validation_log.append("level 0: File valid JSON or YAML")
+    if (level > 0 ):
+        schema = file(args.schema)
+        json_scheam = json.load(schema)
+        validation_log.append("Strating Level 2")
+        try:
+            validate(ymal_data, json_scheam)
+        except ValidationError:
+            for error in ValidationError:
+             print(list(error.path))
+
+
     pass
 
 def convert(args ):
@@ -60,17 +81,19 @@ def main():
 
     default_parser = argparse.ArgumentParser(add_help=False)
 
-
-
     subparsers = parser.add_subparsers(help='sub-command help')
 
     parser_json = subparsers.add_parser('format', parents=[default_parser])
     parser_json.add_argument('format',choices=['json', 'yaml'], help='json  file output path')
     parser_json.add_argument('source', type=str, help='json or yaml file')
     parser_json.add_argument('output', type=str, help='json or yaml file')
-
     parser_json.set_defaults(func=convert)
 
+    parser_validate = subparsers.add_parser('validate', parents=[default_parser])
+    parser_validate.add_argument('source', type=str, help='json or yaml file')
+    parser_validate.add_argument('level',choices=[0, 1],default=1,nargs='?', help='level of validation')
+    parser_validate.add_argument('schema',default='schema/df2_schema.json',nargs='?', help='schema to use to validate. default=')
+    parser_validate.set_defaults(func=df_validate)
 
     # parser_validate = subparsers.add_parser('validate', parents=[default_parser])
     # parser_validate.add_argument('json_schema_file_path', type=str, help='json schema file path')
